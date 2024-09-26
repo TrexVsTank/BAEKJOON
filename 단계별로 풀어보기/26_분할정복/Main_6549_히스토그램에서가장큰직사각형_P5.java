fa9107730b00f46eb70231f928222b37738a45b1
개는또사면돼 <139518005+TrexVsTank@users.noreply.github.com>
2024-09-26 15:37:17 +0900

    Add files via upload

--- 단계별로 풀어보기/26_분할정복/Main_6549_히스토그램에서가장큰직사각형_P5.java
+++ 단계별로 풀어보기/26_분할정복/Main_6549_히스토그램에서가장큰직사각형_P5.java
@@ -1,81 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-// 6549_히스토그램에서 가장 큰 직사각형
-
+// [클래스] 메인
 public class Main_6549_히스토그램에서가장큰직사각형_P5 {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	
+	// [메서드] 메인
 	public static void main(String[] args) throws IOException {
 		
-		StringBuilder sb = new StringBuilder();
+		String input = br.readLine();
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		while (true) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			// N (1 ~ 100,000)
-			int N = Integer.parseInt(st.nextToken());
-			// 0을 입력받으면 종료
-			if (N == 0) {
-				break;
-			}
-			// hist (0 ~ 1,000,000,000)
-			long[] hist = new long[N];
-			for (int i = 0; i < N; i++) {
-				hist[i] = Long.parseLong(st.nextToken());
-			}
+		// 테스트케이스
+		while (!input.equals("0")) {
+			st = new StringTokenizer(input, " ");
 			
-//			N = 100;
-//			hist = new long[N];
-//			for (int i = 0; i < N; i++) {
-//				hist[i] = i+1;
-//			}
+			int N = Integer.parseInt(st.nextToken()); // 막대의 수 (1 <= N <= 100,000)
 			
-			Stack<Integer> stack = new Stack<>();
+			long max_Extent = 0;
 			
-			long max_extent = 0;
-			
-			for (int i = 0; i < N; i++) {
-				while (!stack.isEmpty() && hist[stack.peek()] >= hist[i]) {
-					long height = hist[stack.pop()];
-					long width = 0;
-					if (stack.isEmpty()) {
-						width = i;
-					} else {
-						width = i-stack.peek()-1;
-					}
-					long extent = height * width;
-					if (max_extent < extent) {
-						max_extent = extent;
-					}
+			Stack<long[]> stack = new Stack<>();
+			for (int idx = 1; idx <= N; idx++) {
+				long curr_Height = Integer.parseInt(st.nextToken()); // 막대의 높이 (0 <= h <= 10^9)
+				
+				while (!stack.isEmpty() && stack.peek()[0] >= curr_Height) {
+					
+					long[] popped = stack.pop();
+					long popped_Height = popped[0]; // 높이
+					long width = stack.isEmpty() ? // 넓이
+						idx-1 :
+						idx-1 - stack.peek()[1];
+					max_Extent = Math.max(max_Extent, popped_Height * width);
 				}
-				stack.push(i);
+				stack.push(new long[] {curr_Height, idx});
 			}
-			
-//			System.out.println(Arrays.toString(hist));
-//			System.out.println(stack);
 			
 			while (!stack.isEmpty()) {
-				int index = stack.pop();
-				long height = hist[index];
-				long width = 0;
-				if (stack.isEmpty()) {
-					width = N;
-				} else {
-					width = N-stack.peek()-1;
-				}
-				long extent = height * width;
-				if (max_extent < extent) {
-					max_extent = extent;
-				}
+				long[] popped = stack.pop();
+				long popped_Height = popped[0]; // 높이
+				long width = stack.isEmpty() ?
+					N :
+					N - stack.peek()[1]; // 넓이
+				max_Extent = Math.max(max_Extent, popped_Height * width);
 			}
 			
-			sb.append(max_extent).append("\n");
-		}
+			sb.append(max_Extent).append("\n");
+			
+			input = br.readLine();
+			
+		} // 테스트케이스
 		
+		// 출력
 		System.out.println(sb);
-	}
-}
+		
+	} // [메서드] 메인
+	
+	
+} // [클래스] 메인
