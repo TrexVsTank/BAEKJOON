73c4415b4d2be8e809f0cfe2ba912ab9778900be
개는또사면돼 <139518005+TrexVsTank@users.noreply.github.com>
2024-09-26 16:31:59 +0900

    Add files via upload

--- 단계별로 풀어보기/34_동적계획법과최단거리역추적/Main_14003_가장긴증가하는부분수열5_P5.java
+++ 단계별로 풀어보기/34_동적계획법과최단거리역추적/Main_14003_가장긴증가하는부분수열5_P5.java
@@ -6,62 +6,69 @@ import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-// 클래스
+// [클래스] 메인
 public class Main_14003_가장긴증가하는부분수열5_P5 {
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	
-	// 메인
+	// [메서드] 메인
 	public static void main(String[] args) throws IOException {
 		
-		int N = Integer.parseInt(br.readLine()); // 1 ~ 1_000_000_000
+		int N = Integer.parseInt(br.readLine()); // 수열의 크기 (1 ~ 10^9)
 		
-		int[] arr = new int[N+1]; // -1_000_000_000 ~ 1_000_000_000
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		st = new StringTokenizer(br.readLine(), " ");
+		int[] arr = new int[N+1]; // 수열 (-10^9 ~ 10^9)
 		for (int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int[] arr_idx = new int[N+1];
+		int[] arr_order = new int[N+1]; // 순서 기록 (역추적용)
 		
-		List<Integer> list = new ArrayList<>();
-		list.add(-1_000_000_001);
+		// Longest Increase Sequence
+		List<Integer> LIS = new ArrayList<>(); LIS.add(-1_000_000_001);
 		for (int i = 1; i <= N; i++) {
-			if (arr[i] > list.get(list.size()-1)) {
-				arr_idx[i] = list.size();
-				list.add(arr[i]);
-			} else {
-				int left = 1;
-				int right = list.size()-1;
-				while (left <= right) {
-					int mid = (left+right)/2;
-					if (list.get(mid) < arr[i]) {
-						left = mid+1;
-					} else {
-						right = mid-1;
-					}
-				}
-				arr_idx[i] = left;
-				list.set(left, arr[i]);
+			int curr_Num = arr[i];
+			
+			if (LIS.get(LIS.size()-1) < curr_Num) {
+				arr_order[i] = LIS.size();
+				LIS.add(curr_Num);
 			}
+			
+			int left = 1;
+			int right = LIS.size()-1;
+			while (left <= right) {
+				int mid = (left+right)/2;
+				if (LIS.get(mid) < curr_Num) {
+					left = mid+1;
+				} else {
+					right = mid-1;
+				}
+			}
+			arr_order[i] = left;
+			LIS.set(left, curr_Num);
 		}
 		
-		int len = list.size()-1;
+		// 최대길이
+		int val = LIS.size()-1;
+		sb.append(val).append("\n");
+		
+		// 역추적
 		Stack<Integer> stack = new Stack<>();
 		for (int i = N; i >= 1; i--) {
-			if (arr_idx[i] == len) {
+			if (arr_order[i] == val) {
 				stack.push(arr[i]);
-				len--;
+				val--;
 			}
 		}
-		
-		sb.append(list.size()-1).append("\n");
 		while (!stack.isEmpty()) {
 			sb.append(stack.pop()).append(" ");
 		}
+		
+		// 출력
 		System.out.println(sb);
 		
-	} // 메인
+	} // [메서드] 메인
 	
-} // 클래스
+} // [클래스] 메인
